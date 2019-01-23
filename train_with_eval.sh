b49003c98256e9db48a1e28ea8e84984b4f4b7d2
shigenius <k178578@ie.u-ryukyu.ac.jp>
2019-01-23 20:24:09 +0900

    modify some files

--- train_with_eval.sh
+++ train_with_eval.sh
@@ -1,10 +1,11 @@
-# python train.py --num_batches 10000 --batch_size 20;
-# python evaluate.py --checkpoint_name model.ckpt-10000>hoge.txt;
-TEXT=cat hoge.txt | grep "num of record"
+python train.py --num_batches 10000 --batch_size 20;
+python evaluate.py --checkpoint_name model.ckpt-10000>hoge.txt;
+TEXT=$(cat hoge.txt | grep "num of record")
+
 # push notification to my slack user
 # 設定
 URL='https://hooks.slack.com/services/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'
-# TEXT='Train and evaluation was DONE!'
+TEXT='Train and evaluation was DONE! \n'$TEXT
 USERNAME='hoge'
 LINK_NAMES='1'
 
