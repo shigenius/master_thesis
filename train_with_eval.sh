7913504abc6fb5ade0f5eaba2a3e7dfc1581a65f
shigenius <k178578@ie.u-ryukyu.ac.jp>
2019-01-24 15:21:32 +0900

    implement augmentation in lenet_preprocessing.py

--- train_with_eval.sh
+++ train_with_eval.sh
@@ -1,18 +1,38 @@
-python train.py --num_batches 1000 --batch_size 20;
-python evaluate.py --checkpoint_name model.ckpt-1000>hoge.txt;
-TEXT=$(cat hoge.txt | grep "num of record")
+# python train.py --num_batches 1000 --batch_size 20;
+# python evaluate.py --checkpoint_name model.ckpt-1000>hoge.txt;
+# TEXT=$(cat hoge.txt | grep "num of record")
 
 # push notification to my slack user
 # 設定
 URL='https://hooks.slack.com/services/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'
-TEXT='Train and evaluation was DONE! \n'$TEXT
+# TEXT='Train and evaluation was DONE! \n'$TEXT
 USERNAME='train_with_eval.sh'
 LINK_NAMES='1'
 
-# post
-curl="curl -X POST --data '{ \
-    \"text\": \"${TEXT}\" \
-    ,\"username\": \"${USERNAME}\" \
-    ,\"link_names\" : ${LINK_NAMES}}' \
-    ${URL}"
-eval ${curl}
+# # post
+# curl="curl -X POST --data '{ \
+#     \"text\": \"${TEXT}\" \
+#     ,\"username\": \"${USERNAME}\" \
+#     ,\"link_names\" : ${LINK_NAMES}}' \
+#     ${URL}"
+# eval ${curl}
+
+
+for ((i=0 ; i<10 ; i++))
+do
+  python train.py --num_batches 1000 --batch_size 20;
+  python evaluate.py --checkpoint_name model.ckpt-1000 > hoge.txt;
+  TEXT=$(cat hoge.txt | grep "num of record")
+  TEXT=$i'Train and evaluation was DONE! \n'$TEXT
+
+  # post
+  curl="curl -X POST --data '{ \
+      \"text\": \"${TEXT}\" \
+      ,\"username\": \"${USERNAME}\" \
+      ,\"link_names\" : ${LINK_NAMES}}' \
+      ${URL}"
+  eval ${curl}
+
+done
+
+
